0ccbf995f909d58d892123b6024e3742f27f3d0e
SamCantCode2 <samhruth@gmail.com>
2026-03-02 02:58:35 +0530

    Added basic orchestrator call

--- OrchestratorCall.py
+++ OrchestratorCall.py
@@ -2,14 +2,14 @@ import os
 import json
 from mistralai import Mistral
 
-client = Mistral(api_key="JatrHVB1dyMjeBy29pZZf8XEQzsGVzKG")
+client = Mistral(api_key="")
 
 inputs = [
     {"role":"user","content":"Get me information on the following protein: MRTQLAVKDPGSIYFWNLEHCRGTQVAQMLKDYEPNTIRGHWLVSSKADQYFVKETLPRGIMNQW"}
 ]
 
 response = client.beta.conversations.start(
-    agent_id="ag_019ca82c89e17476ad204bb0b414dbb0",
+    agent_id="",
     inputs=inputs)
 
 entries = response.outputs[0]
